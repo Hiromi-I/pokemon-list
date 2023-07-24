e81945d0755b96dbfeb56fe7354d3a6395563eca
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2023-07-24 23:39:58 +0900

    モンスター情報のデザインを調整

--- src/app/components/MonsterCard.tsx
+++ src/app/components/MonsterCard.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { Suspense } from "react";
 
 import { getPokemonDetailData } from "@/app/utils/poke-api";
-import { Suspense } from "react";
+import TypeLabels from "@/app/components/TypeLabels";
+import AbilityLabels from "@/app/components/AbilityLabels";
+import MonsterDataLabel from "@/app/components/MonsterDataLabel";
 
 type Props = {
   url: string,
@@ -12,14 +15,32 @@ const AsyncMonsterCard = async (props: Props) => {
 
   return (
     <>
-      <div className="h-[171px] w-full p-[3px] mb-4  bg-gradient-to-b from-gray-200 to-gray-400 flex justify-center items-center">
+      <div className="h-[171px] w-full p-[3px] bg-gradient-to-b from-gray-200 to-gray-400 flex justify-center items-center">
         <Image src={sprites.other["official-artwork"].front_default} alt={name} width={165} height={165} priority className="h-[165px] w-[165px]" />
       </div>
-      <h2 className="font-bold mb-3 px-2">{name}</h2>
-      <p className="px-2">高さ: {height}m</p>
-      <p className="px-2">重さ: {weight}kg</p>
-      <p className="px-2">タイプ: {types.join(" / ")}</p>
-      <p className="px-2">能力: {abilities.join(" / ")}</p>
+      <div className="p-3">
+        <h2 className="font-bold mb-3">[No.{id}] {name}</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th><MonsterDataLabel title="高さ" /></th>
+              <td>{height}m</td>
+            </tr>
+            <tr>
+              <th><MonsterDataLabel title="重さ" /></th>
+              <td>{weight}kg</td>
+            </tr>
+            <tr>
+              <th className="align-top"><MonsterDataLabel title="タイプ" /></th>
+              <td><TypeLabels types={types} /></td>
+            </tr>
+            <tr>
+              <th className="align-top"><MonsterDataLabel title="特性" /></th>
+              <td><AbilityLabels abilities={abilities} /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
@@ -30,16 +51,36 @@ const SkeltonCard = () => {
       <div className="h-[171px] w-full mb-4 bg-gradient-to-b from-gray-200 to-gray-400 flex justify-center items-center">
         <Image src="/monster_ball.svg" alt="モンスターボール" width={70} height={70} priority className="h-[70px] w-[165px] animate-bounce" />
       </div>
-      <h2 className="font-bold mb-3 px-2">Loading...</h2>
-      <p className="px-2">高さ: -- m</p>
-      <p className="px-2">重さ: -- kg</p>
+      <div className="p-3">
+        <h2 className="font-bold mb-3">[No.--] Loading...</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th><MonsterDataLabel title="高さ" /></th>
+              <td>--m</td>
+            </tr>
+            <tr>
+              <th><MonsterDataLabel title="重さ" /></th>
+              <td>--kg</td>
+            </tr>
+            <tr>
+              <th className="align-top"><MonsterDataLabel title="タイプ" /></th>
+              <td>--</td>
+            </tr>
+            <tr>
+              <th className="align-top"><MonsterDataLabel title="特性" /></th>
+              <td>--</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
 
 const MonsterCard = (props: Props) => {
   return (
-    <li className="bg-gray-100 text-black text-sm rounded-lg h-[360px] drop-shadow-md overflow-hidden">
+    <li className="bg-gray-100 text-black text-sm rounded-lg h-[380px] drop-shadow-md overflow-hidden">
       <Suspense fallback={<SkeltonCard />}>
         <AsyncMonsterCard url={props.url} />
       </Suspense>
